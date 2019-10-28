0c36694443aaa939dd49b228ebc4b3607a626260
zaitsvnast <zaitsvnast@gmail.com>
2019-10-28 21:44:30 +0700

    local_rus is done

---
+++ local_rus.py
@@ -0,0 +1,9 @@
+# Localization for Russian language.
+
+TХT_PERIOD_OF_INVESTMENT = 'Срок размещения капитала (лет): '
+TXT_INITIAL_CAPITAL = "Начальный капитал ($): "
+TXT_INTEREST_RATE = "Процентная ставка (%/месяц): "
+TXT_INVESTMENT_INFUSION = "Инвестиционные вливания ($/месяц): "
+TXT_YEAR = "год"
+TXT_TABLE_1 = ("|{:^7}|{:^15}|{:^15}|{:^15}|".format("", "основа", "сумма %", ""))
+TXT_TABLE_2 = ("|{:^7}|{:^15}|{:^15}|{:^15}|".format("месяц", "инвестиций", "за месяц", "капитал"))
