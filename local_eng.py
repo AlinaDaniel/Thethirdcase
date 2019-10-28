719721842a0d216e1a3bc950cddf68123b30e74c
Настя <a.zemtseva@g.nsu.ru>
2019-10-28 21:58:36 +0700

    localisation

--- local_eng.py
+++ local_eng.py
@@ -5,4 +5,5 @@ TXT_INITIAL_CAPITAL = "Initial capital ($): "
 TXT_INTEREST_RATE = "Interest rate (%/month): "
 TXT_INVESTMENT_INFUSION = "Investment infusion ($/month): "
 TXT_YEAR = "year"
-
+TXT_TABLE_1 = ("|{:^7}|{:^15}|{:^15}|{:^15}|".format("", "basis", "amount %", ""))
+TXT_TABLE_2 = ("|{:^7}|{:^15}|{:^15}|{:^15}|".format("month", "investment", "per month", "capital"))
---
+++ local_fr.py
@@ -0,0 +1,9 @@
+# Localization for French language.
+
+TХT_PERIOD_OF_INVESTMENT = 'Durée de placement du capital (années): '
+TXT_INITIAL_CAPITAL = "Capital initial ($): "
+TXT_INTEREST_RATE = "Taux d'intérêt (% / mois): "
+TXT_INVESTMENT_INFUSION = "Injections d'investissement ($ / mois): "
+TXT_YEAR = "year"
+TXT_TABLE_1 = ("|{:^7}|{:^15}|{:^15}|{:^15}|".format("", "base", "montant %", ""))
+TXT_TABLE_2 = ("|{:^7}|{:^15}|{:^15}|{:^15}|".format("mois", "investissement", "par mois", "capitale"))
